9fc72ff4aa1e1250abe95ebfcbf6f7e6ee02007c
George Karaszi <georgekaraszi@gmail.com>
2018-11-12 15:48:35 -0800

    reduce extconf building setup

--- ext/fast_diff_match_patch/extconf.rb
+++ ext/fast_diff_match_patch/extconf.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "mkmf"
-require "rbconfig"
 
 extension_name = "fast_diff_match_patch"
 
@@ -16,17 +15,14 @@ platform = RUBY_PLATFORM
 version  = RUBY_VERSION.split(".")
 puts ">>>>> Creating Makefile for #{type} version #{RUBY_VERSION} on #{platform} <<<<<"
 
-dflags = {
-  "RUBY_TYPE" => type,
-  (type.upcase + "_RUBY")              => nil,
-  "RUBY_VERSION"                       => RUBY_VERSION,
-  "RUBY_VERSION_MAJOR"                 => version[0],
-  "RUBY_VERSION_MINOR"                 => version[1],
-  "RUBY_VERSION_MICRO"                 => version[2],
-  "RSTRUCT_LEN_RETURNS_INTEGER_OBJECT" => type == "ruby" && version[0] == "2" && version[1] == "4" && version[2] <= "1" ? 1 : 0
-}
-
-dflags.each do |k, v|
+{
+  (type.upcase + "_RUBY") => nil,
+  "RUBY_TYPE"             => type,
+  "RUBY_VERSION"          => RUBY_VERSION,
+  "RUBY_VERSION_MAJOR"    => version[0],
+  "RUBY_VERSION_MINOR"    => version[1],
+  "RUBY_VERSION_MICRO"    => version[2]
+}.each_pair do |k, v|
   $CPPFLAGS += if v.nil?
     " -D#{k}"
   else
